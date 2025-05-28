e3a6fd8b0320de64f8e3309dc1cda3491c7385f9
Ralitsa Gercheva <138673469+rali-g@users.noreply.github.com>
2025-05-28 21:21:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,5 @@
-#include "../../UsersHierarchy/Employee.h"
-#include "../../UsersHierarchy/Cashier.h"
-#include "../../Helpers/MyVector.hpp"
-#include "../../UsersHierarchy/Manager.h"
-#include "../../Components/CodeGenerator.hpp"
-#include "../../Components/Transaction.h"
-#include "../../Supermarket/Supermarket.h"
-#include "../../ProductsHierarchy/ProductsByUnit.h"
-#include "../../ProductsHierarchy/ProductsByWeight.h"
-#include "../../Components/Warning.h"
-#include "../../../Supermarket/Supermarket/Supermarket.h"
-#include "../../Supermarket/SellProduct.h"
-#include "../../CommandsHierarchy/Command.hpp"
-#include "../../CommandsHierarchy/CommandFactory.h"
 #include "../../SupermarketLauncher/SupermarketLauncher.h"
-#include <ctime>
 #include <iostream>
-using namespace std;
 
 int main() {
     try {
